725c9521e5555d5f9fc2cfd1d38144ded0f4f379
Michael Claybaugh <michael.claybaugh@gmail.com>
2020-09-08 23:05:22 -0400

    basic firebase integration

--- App.tsx
+++ App.tsx
@@ -2,6 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import * as firebaseConfig from "./firebase.config.json";
+
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
   return (
     <View style={styles.container}>
